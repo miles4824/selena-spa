62c2a3832a743f0b4377dfde39f0208ab9a6f24b
Miles <miles4824@users.noreply.github.com>
2026-09-03 14:18:20 +0700

    Update pos-custom-dropdown-items CSS classes: replace p-2 with border-[#EFE8DF] border-t pb-2 px-2; bump to v0.1.3.3

--- js/config.js
+++ js/config.js
@@ -80,7 +80,7 @@ function parseBirthMonth(val) {
   return 0;
 }
 
-const APP_VERSION = 'v0.1.3.2';
+const APP_VERSION = 'v0.1.3.3';
 // =============================================================
 // SELENA SPA - GLOBAL CONFIG & CONSTANTS
 // =============================================================
